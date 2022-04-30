b92f9eb8ad9fd77602d45acf7af217d2c9f058bb
pierrek6e <pierrecass75@gmail.com>
2022-04-30 22:58:18 +0700

    1st version

---
+++ script.js
@@ -0,0 +1,67 @@
+const timer = document.getElementById("timer");
+const score = document.getElementById("score");
+const finalScore = document.getElementById("final-score");
+const word = document.getElementById("word");
+const input = document.querySelector("input");
+const gameContainer = document.getElementById("game-container");
+const endGameContainer = document.getElementById("reload-container");
+const words = ["hello", "house", "chiken", "written", "escape", "application"];
+
+var countdown = setInterval(runTimer, 1000);
+var time = 10;
+endGameContainer.style.display = "none";
+
+input.addEventListener("input", () => compare());
+
+function compare() {
+  if (word.innerText.toUpperCase() === input.value.toUpperCase()) {
+    incrementScore();
+    setTimer();
+    showNextWord();
+  }
+}
+
+function setTimer() {
+  const difficulty = document.getElementById("difficulty-level");
+  time = 2 * difficulty.value;
+}
+
+function showNextWord() {
+  function getWord() {
+    let nextWord = words[Math.floor(Math.random() * words.length)];
+    return nextWord === word.innerText ? getWord() : nextWord;
+  }
+  word.innerText = getWord(); 
+  input.value = "";
+}
+
+function incrementScore() {
+  score.innerText = parseInt(score.innerText) + 1;
+}
+
+function runTimer() {
+    time--;
+    if (time >= 0) {
+      timer.innerText = time;
+    } else {
+      showEndGame();
+    }
+}
+
+function showEndGame() {
+  gameContainer.style.display = "none";
+  endGameContainer.style.display = "block";
+  finalScore.innerText = score.innerText;
+  clearInterval(countdown);
+  countdown = null;
+}
+
+function reload() {
+  gameContainer.style.display = "block";
+  endGameContainer.style.display = "none";
+  score.innerText = "0";
+  finalScore.innerText = "0";
+  input.value = "";
+  setTimer();
+  countdown = setInterval(runTimer, 1000);
+}
